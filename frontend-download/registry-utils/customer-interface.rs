bf421ab91f01dc7a2a15ff98508a2e33549ae4ca
donblackk2 <epifan_g5903537@rambler.ru>
2025-12-05 10:38:45 +0800

    add customer-interface.rs, IFHEVMExecutor.dbg.json, hooks-order-types.go

---
+++ frontend-download/registry-utils/customer-interface.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend-download\registry-utils\customer-interface.rs
+// Generated at: 2025-10-25T11:02:50.518Z
---
+++ frontend/web/src/download-secure-pages/hooks-order-types.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\download-secure-pages\hooks-order-types.go
+// Generated at: 2025-10-25T11:02:50.569Z
